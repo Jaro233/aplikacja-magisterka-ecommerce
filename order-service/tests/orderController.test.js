0c6022bc5eea5ff1de8cf3893876fc05cfe890a3
Jaro233 <jaroslaw.waliczek@vp.pl>
2024-05-22 19:11:23 +0200

    test

--- order-service/tests/orderController.test.js
+++ order-service/tests/orderController.test.js
@@ -2,15 +2,6 @@ const request = require("supertest");
 const { app, redisClient } = require("../server");
 
 describe("Order Microservice", () => {
-  beforeAll((done) => {
-    done();
-  });
-
-  afterAll((done) => {
-    // Closing the DB connection allows Jest to exit successfully.
-    redisClient.quit();
-    done();
-  });
   it("should return 200 for health check", async () => {
     const res = await request(app).get("/health");
     expect(res.statusCode).toEqual(200);
