70e53eb0e73b3cbfe110f939dcd52e76ac554c01
Jaro233 <jaroslaw.waliczek@vp.pl>
2024-05-22 15:43:54 +0200

    test

--- user-service/server.js
+++ user-service/server.js
@@ -16,15 +16,17 @@ app.get("/health", (req, res) => {
   res.status(200).json({ status: "UP" });
 });
 
-sequelize
-  .sync()
-  .then(() => {
-    app.listen(5001, () => {
-      console.log("User service running on port 5001");
+if (process.env.NODE_ENV !== "test") {
+  sequelize
+    .sync()
+    .then(() => {
+      app.listen(5001, () => {
+        console.log("User service running on port 5001");
+      });
+    })
+    .catch((err) => {
+      console.error("Unable to connect to the database:", err);
     });
-  })
-  .catch((err) => {
-    console.error("Unable to connect to the database:", err);
-  });
+}
 
 module.exports = app;
