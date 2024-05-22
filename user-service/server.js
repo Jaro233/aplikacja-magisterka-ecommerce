4efa9f9ac7630c79539503c664d12eb32c04102e
Jaro233 <jaroslaw.waliczek@vp.pl>
2024-05-22 15:28:57 +0200

    test

--- user-service/server.js
+++ user-service/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const sequelize = require("./config/database");
+const { sequelize } = require("./models");
 const userRoutes = require("./routes/user");
 require("dotenv").config();
 
@@ -16,13 +16,17 @@ app.get("/health", (req, res) => {
   res.status(200).json({ status: "UP" });
 });
 
-sequelize
-  .sync()
-  .then(() => {
-    app.listen(5001, () => {
-      console.log("User service running on port 5001");
+if (process.env.NODE_ENV !== "test") {
+  sequelize
+    .sync()
+    .then(() => {
+      app.listen(5001, () => {
+        console.log("User service running on port 5001");
+      });
+    })
+    .catch((err) => {
+      console.error("Unable to connect to the database:", err);
     });
-  })
-  .catch((err) => {
-    console.error("Unable to connect to the database:", err);
-  });
+}
+
+module.exports = app;
